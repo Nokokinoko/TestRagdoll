1f93519d38260056908d7e65b21ff86fb9b6d026
Syota Tsuda <s-tsuda@magicant.co.jp>
2021-11-22 14:29:05 +0900

    update addforce and reset button, add tmp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,56 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    private Animator m_Animator;
-    private readonly List<Rigidbody> m_ListRigidbody = new List<Rigidbody>();
+	[SerializeField] private Rigidbody m_CenterOfGravity;
+	
+	private Animator m_Animator;
+	private readonly List<Rigidbody> m_ListRigidbody = new List<Rigidbody>();
 
-    private void Awake()
-    {
-        m_Animator = GetComponent<Animator>();
-        
-        foreach (Rigidbody _Child in GetComponentsInChildren<Rigidbody>())
-        {
-            if (transform != _Child.transform)
-            {
-                m_ListRigidbody.Add(_Child);
-            }
-        }
-    }
+	public bool IsRagdoll { get; private set; }
 
-    public void ToRagdoll()
-    {
-        foreach (Rigidbody _Child in m_ListRigidbody)
-        {
-            _Child.isKinematic = false;
-        }
+	private void Awake()
+	{
+		m_Animator = GetComponent<Animator>();
+		IsRagdoll = false;
+		
+		foreach (Rigidbody _Child in GetComponentsInChildren<Rigidbody>())
+		{
+			if (transform != _Child.transform)
+			{
+				m_ListRigidbody.Add(_Child);
+			}
+		}
+	}
 
-        m_Animator.enabled = false;
-    }
+	public void ToRagdoll()
+	{
+		IsRagdoll = true;
+		
+		foreach (Rigidbody _Child in m_ListRigidbody)
+		{
+			_Child.isKinematic = false;
+		}
 
-    public void ToAnimate()
-    {
-        foreach (Rigidbody _Child in m_ListRigidbody)
-        {
-            _Child.isKinematic = true;
-        }
+		m_Animator.enabled = false;
+	}
 
-        m_Animator.enabled = true;
-    }
+	public void ToAnimate()
+	{
+		IsRagdoll = false;
+		
+		foreach (Rigidbody _Child in m_ListRigidbody)
+		{
+			_Child.isKinematic = true;
+		}
+
+		m_Animator.enabled = true;
+	}
+
+	public void AddForce(Vector2 pNormalizedInput)
+	{
+		m_CenterOfGravity.AddForce(
+			new Vector3(pNormalizedInput.x, 0.0f, pNormalizedInput.y) * 20.0f,
+			ForceMode.Acceleration
+		);
+	}
 }
--- Assets/Scripts/SceneConductor.cs
+++ Assets/Scripts/SceneConductor.cs
@@ -1,19 +1,49 @@
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneConductor : MonoBehaviour
 {
-    [SerializeField] private PlayerController m_CtrlPlayer;
+	[SerializeField] private PlayerController m_CtrlPlayer;
 
-    private void Update()
-    {
-        switch (InputManager.GetTouch())
-        {
-            case ENUM_TOUCH.TOUCH_BEGAN:
-                m_CtrlPlayer.ToRagdoll();
-                break;
-            case ENUM_TOUCH.TOUCH_ENDED:
-                m_CtrlPlayer.ToAnimate();
-                break;
-        }
-    }
+	[SerializeField] private Button m_BtnReset;
+
+	private Vector2 m_TouchBeagn = Vector2.zero;
+
+	private void Awake()
+	{
+		m_BtnReset.OnClickAsObservable()
+			.Where(_ => m_CtrlPlayer.IsRagdoll)
+			.Subscribe(_ => m_CtrlPlayer.ToAnimate())
+			.AddTo(this)
+		;
+		
+		this.UpdateAsObservable()
+			.Subscribe(_ => {
+				switch (InputManager.GetTouch())
+				{
+					case ENUM_TOUCH.TOUCH_BEGAN:
+						if (m_CtrlPlayer.IsRagdoll)
+						{
+							m_TouchBeagn = InputManager.GetPosition();
+						}
+						else
+						{
+							m_CtrlPlayer.ToRagdoll();
+						}
+						break;
+					case ENUM_TOUCH.TOUCH_MOVED:
+						if (m_TouchBeagn != Vector2.zero)
+						{
+							m_CtrlPlayer.AddForce((InputManager.GetPosition() - m_TouchBeagn).normalized);
+						}
+						break;
+					case ENUM_TOUCH.TOUCH_ENDED:
+						m_TouchBeagn = Vector2.zero;
+						break;
+				}
+			})
+		;
+	}
 }
